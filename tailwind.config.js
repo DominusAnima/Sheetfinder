33483a0e19f325f506a6402adc7e8de46969f37e
Valter Martinek <martinek@freevision.sk>
2023-09-05 19:11:18 +0200

    Add primary and secondary colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
+/*eslint-env node*/
+import colors from "tailwindcss/colors";
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: colors.yellow,
+        secondary: colors.orange,
+      },
+    },
   },
   plugins: [],
 };
