007d1acb674a6b4e2d99e1c194b3c0bd1d61cc0d
DominusAnima <andrejman1@gmail.com>
2023-10-25 12:47:40 +0200

    Formatted Feats block

--- src/Blocks/Feats.tsx
+++ src/Blocks/Feats.tsx
@@ -1,64 +1,99 @@
-import { Feat, FeatBlock } from "../charSheet";
+import { useState } from "react";
+import { FeatBlock } from "../charSheet";
 import { useFormDispatch } from "../lib/useFormDispatch";
+import SectionTitle from "../Components/SectionTitle";
+import EditButton from "../Components/EditButton";
+import FlatButton from "../Components/FlatButton";
+import { FaPlusCircle } from "react-icons/fa";
+import Button from "../Components/Button";
+import InlineInput from "../Components/InlineInput";
 
 export function Feats({ state }: { state: FeatBlock }) {
+  const [editing, setEditing] = useState(false);
   const dispatch = useFormDispatch();
-
-  function handleSubmit(event: { preventDefault: any }) {
-    event.preventDefault;
-  }
-
-  function addEntry() {
-    dispatch({ type: "addFeatEntry" });
-  }
-
-  function removeEntry(entry: Feat) {
-    dispatch({ type: "removeFeatEntry", payload: { entry } });
-  }
-
-  function handleClick(index: number) {
-    dispatch({ type: "toggleFeatDetail", payload: { index } });
-  }
 
   function handleChange(field: "name" | "description", index: number, value: string) {
     dispatch({ type: "changeFeatEntryField", payload: { field, index, value } });
   }
 
   return (
-    <div onSubmit={handleSubmit}>
-      <h2>Feats & Features</h2>
-      <button onClick={addEntry}>Add Entry</button>
-      <table>
+    <div className="mt-4">
+      <SectionTitle title="Feats & Features">
+        <EditButton editing={editing} onClick={() => setEditing((v) => !v)} />
+        <FlatButton onClick={() => dispatch({ type: "addFeatEntry" })}>
+          <FaPlusCircle />
+        </FlatButton>
+      </SectionTitle>
+
+      <table className="table table--striped w-full mt-4">
         <thead>
           <tr>
-            <th>Toggle Description</th>
-            <th>Name</th>
+            <th className="whitespace-nowrap">Description</th>
+            <th className="whitespace-nowrap">Name</th>
           </tr>
         </thead>
-        <tbody>
-          {state.entries.map((entry, i) => (
-            <>
-              <tr>
-                <td>
-                  <button onClick={() => handleClick(i)}>Toggle</button>
-                </td>
-                <td>
-                  <input value={entry.name} onChange={(e) => handleChange("name", i, e.target.value)} />
-                </td>
-                <td>
-                  <button onClick={() => removeEntry(entry)}>Remove</button>
-                </td>
-              </tr>
-              {entry.toggleDescr && (
+        {editing ? (
+          <tbody>
+            {state.entries.map((entry, i) => (
+              <>
                 <tr>
                   <td>
-                    <input value={entry.description} onChange={(e) => handleChange("description", i, e.target.value)} />
+                    <Button size="small" onClick={() => dispatch({ type: "toggleFeatDetail", payload: { index: i } })}>
+                      Toggle
+                    </Button>
+                  </td>
+                  <td>
+                    <InlineInput value={entry.name} onChange={(e) => handleChange("name", i, e.target.value)} />
+                  </td>
+                  <td>
+                    <Button
+                      size="small"
+                      onClick={() => {
+                        if (confirm("Are you sure?")) {
+                          dispatch({ type: "removeFeatEntry", payload: { entry } });
+                        }
+                      }}
+                    >
+                      Remove
+                    </Button>
                   </td>
                 </tr>
-              )}
-            </>
-          ))}
-        </tbody>
+                {entry.toggleDescr && (
+                  <tr>
+                    <td colSpan={2}>
+                      <InlineInput
+                        value={entry.description}
+                        onChange={(e) => handleChange("description", i, e.target.value)}
+                      />
+                    </td>
+                  </tr>
+                )}
+              </>
+            ))}
+          </tbody>
+        ) : (
+          <tbody>
+            {state.entries.map((entry, i) => (
+              <>
+                <tr>
+                  <td>
+                    <Button size="small" onClick={() => dispatch({ type: "toggleFeatDetail", payload: { index: i } })}>
+                      Toggle
+                    </Button>
+                  </td>
+                  <td className="text-center">{entry.name}</td>
+                </tr>
+                {entry.toggleDescr && (
+                  <tr>
+                    <td colSpan={2} className="text-center">
+                      {entry.description}
+                    </td>
+                  </tr>
+                )}
+              </>
+            ))}
+          </tbody>
+        )}
       </table>
     </div>
   );
