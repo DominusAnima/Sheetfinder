5d7f358c9e5651645fb0e739dbd6b734c49b3cae
DominusAnima <andrejman1@gmail.com>
2023-10-25 12:12:21 +0200

    Formatted Skills block.

--- src/Blocks/Skills.tsx
+++ src/Blocks/Skills.tsx
@@ -1,8 +1,17 @@
+import { useState } from "react";
 import { Abilities, Blocks } from "../charSheet";
 import { ABILITY_TYPES } from "../constants";
 import { useFormDispatch } from "../lib/useFormDispatch";
+import SectionTitle from "../Components/SectionTitle";
+import EditButton from "../Components/EditButton";
+import FlatButton from "../Components/FlatButton";
+import { FaPlusCircle } from "react-icons/fa";
+import Field from "../Components/Field";
+import InlineInput from "../Components/InlineInput";
+import Button from "../Components/Button";
 
 export function Skills({ state }: { state: Blocks }) {
+  const [showDetails, setShowDetails] = useState(false);
   const dispatch = useFormDispatch();
   function handleChange(skillIndex: number, field: "name" | "ranks" | "misc", value: string) {
     if (
@@ -24,63 +33,56 @@ export function Skills({ state }: { state: Blocks }) {
     dispatch({ type: "toggleSkill", payload: { skillIndex, field } });
   }
 
-  function handleSubmit(event: { preventDefault: any }) {
-    event.preventDefault;
-  }
-
-  function toggleDetail() {
-    dispatch({ type: "toggleSkillDetail" });
-  }
-
-  function addSkill() {
-    dispatch({ type: "addSkill" });
-  }
-
   function handleSelectChange(index: number, value: keyof Abilities) {
     dispatch({ type: "changeSkillAbil", payload: { index, value } });
   }
 
-  function removeSkill(skillIndex: number) {
-    dispatch({ type: "removeSkill", payload: { skillIndex } });
-  }
-
   return (
-    <div onSubmit={handleSubmit}>
-      <h2>Skills</h2>
-      <button onClick={toggleDetail}>toggle detail</button>
-      <button onClick={addSkill}>Add Skill</button>
-      <label>
-        available ranks:{state.skills.remainRanks}/{state.skills.totalRanks}
-      </label>
-      <table>
+    <div className="mt-4">
+      <SectionTitle title="Skills">
+        <EditButton editing={showDetails} onClick={() => setShowDetails((v) => !v)} />
+        <FlatButton onClick={() => dispatch({ type: "addSkill" })}>
+          <FaPlusCircle />
+        </FlatButton>
+      </SectionTitle>
+
+      <Field label="Available ranks: " horizontal>
+        <p className="flex-1 text-center value">
+          {state.skills.remainRanks}/{state.skills.totalRanks}
+        </p>
+      </Field>
+
+      <hr className="my-4" />
+
+      <table className="table table--striped w-full mt-4">
         <thead>
-          {state.skills.detailToggle ? (
+          {showDetails ? (
             <tr>
-              <th>trained</th>
-              <th>class</th>
-              <th>skill name</th>
-              <th>ability</th>
-              <th>total</th>
-              <th>ability bonus</th>
-              <th>AC penalty</th>
-              <th>ranks</th>
-              <th>misc</th>
+              <th className="whitespace-nowrap">trained</th>
+              <th className="whitespace-nowrap">class</th>
+              <th className="whitespace-nowrap">skill name</th>
+              <th className="whitespace-nowrap">ability</th>
+              <th className="whitespace-nowrap">total</th>
+              <th className="whitespace-nowrap">ability bonus</th>
+              <th className="whitespace-nowrap">AC penalty</th>
+              <th className="whitespace-nowrap">ranks</th>
+              <th className="whitespace-nowrap">misc</th>
             </tr>
           ) : (
             <tr>
-              <th>trained</th>
-              <th>skill name</th>
-              <th>total</th>
+              <th className="whitespace-nowrap">trained</th>
+              <th className="whitespace-nowrap">skill name</th>
+              <th className="whitespace-nowrap">total</th>
             </tr>
           )}
         </thead>
         <tbody>
-          {state.skills.detailToggle
+          {showDetails
             ? state.skills.skills.map((skill, i) => {
                 return (
                   <tr key={i}>
                     {skill.editable ? (
-                      <td>
+                      <td className="text-center">
                         <input
                           type="checkbox"
                           defaultChecked={skill.trained}
@@ -88,24 +90,25 @@ export function Skills({ state }: { state: Blocks }) {
                         />
                       </td>
                     ) : (
-                      <td>{skill.trained ? "*" : ""}</td>
+                      <td className="text-center">{skill.trained ? "*" : ""}</td>
                     )}
-                    <td>
+                    <td className="text-center">
                       <input
                         type="checkbox"
                         defaultChecked={skill.classSkill}
                         onChange={() => handleSkillToggle(i, "classSkill")}
                       />
                     </td>
-                    <td>
-                      {skill.editable ? (
-                        <input value={skill.name} onChange={(e) => handleChange(i, "name", e.target.value)} />
-                      ) : (
-                        skill.name
-                      )}
-                    </td>
-                    <td>
-                      {skill.editable ? (
+                    {skill.editable ? (
+                      <td>
+                        <InlineInput value={skill.name} onChange={(e) => handleChange(i, "name", e.target.value)} />
+                      </td>
+                    ) : (
+                      <td className="text-center font-bold">{skill.name}</td>
+                    )}
+
+                    {skill.editable ? (
+                      <td className="text-center">
                         <select
                           value={skill.ability}
                           onChange={(e) => handleSelectChange(i, e.target.value as keyof Abilities)}
@@ -118,22 +121,25 @@ export function Skills({ state }: { state: Blocks }) {
                             );
                           })}
                         </select>
-                      ) : (
-                        skill.ability
-                      )}
+                      </td>
+                    ) : (
+                      <td className="text-center">{skill.ability}</td>
+                    )}
+
+                    <td className="text-center value">
+                      {!skill.trained || (skill.trained && Number(skill.ranks) > 0) ? skill.totalBonus : "-"}
                     </td>
-                    <td>{!skill.trained || (skill.trained && Number(skill.ranks) > 0) ? skill.totalBonus : "-"}</td>
-                    <td>{state.abilityBlock.abilities[skill.ability].mod}</td>
-                    <td>{skill.armorPenalty ? state.combat.armorCheckPenalty : "-"}</td>
+                    <td className="text-center value">{state.abilityBlock.abilities[skill.ability].mod}</td>
+                    <td className="text-center value">{skill.armorPenalty ? state.combat.armorCheckPenalty : "-"}</td>
                     <td>
-                      <input
+                      <InlineInput
                         type="number"
                         value={skill.ranks}
                         onChange={(e) => handleChange(i, "ranks", e.target.value)}
                       />
                     </td>
                     <td>
-                      <input
+                      <InlineInput
                         type="number"
                         value={skill.misc}
                         onChange={(e) => handleChange(i, "misc", e.target.value)}
@@ -141,7 +147,16 @@ export function Skills({ state }: { state: Blocks }) {
                     </td>
                     {skill.editable && (
                       <td>
-                        <button onClick={() => removeSkill(i)}>Remove</button>
+                        <Button
+                          size="small"
+                          onClick={() => {
+                            if (confirm("Are you sure?")) {
+                              dispatch({ type: "removeSkill", payload: { skillIndex: i } });
+                            }
+                          }}
+                        >
+                          Remove
+                        </Button>
                       </td>
                     )}
                   </tr>
@@ -150,9 +165,11 @@ export function Skills({ state }: { state: Blocks }) {
             : state.skills.skills.map((skill, i) => {
                 return (
                   <tr key={i}>
-                    <td>{skill.trained ? "*" : ""}</td>
-                    <td>{skill.name}</td>
-                    <td>{!skill.trained || (skill.trained && Number(skill.ranks) > 0) ? skill.totalBonus : "-"}</td>
+                    <td className="text-center">{skill.trained ? "*" : ""}</td>
+                    <td className="text-center font-bold">{skill.name}</td>
+                    <td className="text-center value">
+                      {!skill.trained || (skill.trained && Number(skill.ranks) > 0) ? skill.totalBonus : "-"}
+                    </td>
                   </tr>
                 );
               })}
